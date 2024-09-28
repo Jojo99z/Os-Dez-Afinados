1352b80023bdf78d70c6a1456d633cfb2ad5bc50
Joao Conduto <63789202+Jojo99z@users.noreply.github.com>
2024-09-28 22:57:29 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
+      navCLose = document.getElementById('nav-close')
+      
 /* Menu show */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
@@ -10,65 +10,86 @@ if(navToggle){
     })
 }
 
-/* Menu hidden */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+/*Menu hidden */
+if(navCLose){
+    navCLose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
-
+  
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
+    // When we click on each nav__link, we remove the show-menu
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*=============== SWIPER CLOTHING ===============*/
-let swiperHome = new Swiper('.home__swiper', {
-    loop: true,
-    grabCursor: true,
-    slidesPerView: 'auto',
-
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    breakpoints:{
-        768:{
-            slidesPerView: 3,
-            centeredSlides: 'auto',
-        },
-        1152:{
-            centeredSlides: 'auto',
-            spaceBetween: -64,
-        }
-    }
-})
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const bgHeader = () =>{
     const header = document.getElementById('header')
-    // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('bg-header') 
+    // Add a class if the bottom offfset is greater than 50 of the
+    this.scrollY >= 50 ? header.classList.add('bg-header')
                        : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', bgHeader)
+bgHeader()
+
+/*=============== SWIPER SERVICES ===============*/
+const swiperServices = new Swiper('.services__swiper',{
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+     // When the scroll is higher than 350 viewport height, add the
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                    : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+scrollUp()
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId +']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }else{
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '6opx',
-    duration: 2500,
-    //reset:true //Animations repeat
+    distance: '100px',
+    duration:  2500,
+    delay: 400,
+    //reset: true, //Animations repeat
 })
-
-sr.reveal(`home__swiper, .home__footer`)
-sr.reveal('home__circle', {scale: 1.5, delay:300})
-sr.reveal('home__subcircle', {scale: 1.5, delay: 500})
-sr.reveal('home__title',{scale: 1, origin: 'bottom', delay: 1200})
-sr.reveal('.swiper-button-prev, .swiper-button-next', {origin: 'bottom'})
+sr.reveal('.home__content, .services__data, .services__swiper')
+sr.reveal('home__images', {origin: 'bottom', delay: 1000})
+sr.reveal('.about__images, .contact__data',{origin: 'right'})
+sr.reveal('.project__card', {interval: 100})
